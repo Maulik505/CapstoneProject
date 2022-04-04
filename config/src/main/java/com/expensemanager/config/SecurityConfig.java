539999bab41c3433c64e8c1e23015f3039553390
Maulik Mistri (C0783505) <c0783505@mylambton.ca>
2022-04-04 00:40:47 -0400

    config code changes

---
+++ config/src/main/java/com/expensemanager/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.expensemanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *  @author Maulik Mistri
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+            .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .httpBasic()
+                ;
+    }
+}
